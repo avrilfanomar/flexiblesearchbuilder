73de257d2105c88aee14825dd8ade7f8240b39f7
Andrii Andriichuk <aandriichuk@remit.se>
2018-11-17 17:14:03 +0200

    Add generated extension

---
+++ src/org/bitbucket/andriichukandrii/constants/FlexiblesearchbuilderConstants.java
@@ -0,0 +1,28 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package org.bitbucket.andriichukandrii.constants;
+
+/**
+ * Global class for all Flexiblesearchbuilder constants. You can add global constants for your extension into this class.
+ */
+public final class FlexiblesearchbuilderConstants extends GeneratedFlexiblesearchbuilderConstants
+{
+	public static final String EXTENSIONNAME = "flexiblesearchbuilder";
+
+	private FlexiblesearchbuilderConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+
+    public static final String PLATFORM_LOGO_CODE = "flexiblesearchbuilderPlatformLogo";
+}
